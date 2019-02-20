5446957e1362934f98709ebc43154389104f5510
Darren L <darren.lou@gmail.com>
2019-02-20 09:02:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    array.split(",")
+    array.join(",")
     return array.push(-1, "and")
   end
   
