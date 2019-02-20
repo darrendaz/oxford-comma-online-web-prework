acfe4c4ca00a75d723ff3f4d7635b8ece68b60f1
Darren L <darren.lou@gmail.com>
2019-02-20 09:09:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    return "#{array[0..array.length-1].join(', ')} and #{array.last}"
+    return "#{array[0..-2].join(', ')} and #{array.last}"
   end
   
 end
